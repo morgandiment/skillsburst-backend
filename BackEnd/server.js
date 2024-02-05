3a7cf47bc0dd080d531260b536634424337f2508
adrian sive <adrianearlrivera@gmail.com>
2024-02-05 14:13:22 +0000

    added connection

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
-const dataRoutes=  require('./src/routes/dataRoutes.js');
+const cors = require('cors');
+const Routes=  require('./src/routes/Routes.js');
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
-
-app.use('/' , dataRoutes);
+app.use(Routes);
 
 app.listen(port  , () => {
     console.log(`Server is ran on ${port}`);
--- BackEnd/src/routes/dataRoutes.js
+++ BackEnd/src/routes/dataRoutes.js
@@ -4,4 +4,11 @@ const router = express.Router();
 
 dbController.pool.connect()
 
+router.get('/testconnection', (req, res) => {
+    const fakeApiResponse = { message: 'Simulated API response for testing' };
+
+    res.status(200).json({ success: true, data: fakeApiResponse });
+});
+
+
 module.exports = router;

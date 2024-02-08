25c538bdb6ee7baa35cbd5beef8ecd5c1bf048dd
adrian sive <adrianearlrivera@gmail.com>
2024-02-08 13:17:09 +0000

    updating

--- BackEnd/src/routes/dataRoutes.js
+++ BackEnd/src/routes/dataRoutes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const dbController = require('../db/dbController')
 const router = express.Router();
 
-dbController.pool.connect()
+dbController.connectToDatabase();
 
 router.get('/testconnection', (req, res) => {
     const fakeApiResponse = { message: 'Simulated API response for testing' };
 
     res.status(200).json({ success: true, data: fakeApiResponse });
+    console.log('Sucess')
 });
 
+router.get('/testquery', (req,res) => {
+    
+})
 
 module.exports = router;

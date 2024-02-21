faba81dc57d35d711196d3b56faa3a169ded2f08
adrian sive <adrianearlrivera@gmail.com>
2024-02-21 12:33:52 +0000

    added register login

--- BackEnd/src/routes/Routes.js
+++ BackEnd/src/routes/Routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const dataRoutes = require('./dataRoutes');
 const loginRoutes = require('./loginRoutes')
+const registerRoutes = require('./registerRoutes')
 
 //data routes
 router.use('/data',dataRoutes);
@@ -10,5 +11,7 @@ router.use('/data',dataRoutes);
 //login routes
 router.use('/login',loginRoutes);
 
+//register routes
+router.use('/register',registerRoutes);
 
 module.exports = router;
---
+++ BackEnd/src/utils/security.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+
+async function encrypt(value) {
+    
+    let salt = bcrypt.genSaltSync(10)
+    let hash = await bcrypt.hash(value , salt)
+    return hash;
+} 
+
+async function hashMatch(value , hash) {
+    let isMatch = await bcrypt.compare(value,hash)
+    return isMatch
+     
+} 
+
+module.exports = { encrypt, hashMatch };
